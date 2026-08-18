89c7ed5746e9b199e1861f98088148f358ee05b0
nathanwoohooyay <nathanjiang10@gmail.com>
2026-08-18 17:38:05 +0000

    Added formalgreet method.

--- src/Greeter.java
+++ src/Greeter.java
@@ -2,4 +2,8 @@ public class Greeter {
     public String greet(String name) {
         return "Good day, " + name;
     }
+
+    public String formalgreet(String name) {
+        return "Very good day, " + name;
+    }
 }
